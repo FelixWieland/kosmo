ba6e7cadb62defa4ef29776abe5b2b4c6877049d
Felix Wieland <felix.wieland@mail.de>
2020-01-25 15:39:47 +0100

    added combinator tests

--- combinator_test.go
+++ combinator_test.go
@@ -3,11 +3,52 @@ package kosmo
 import (
 	"testing"
 
+	"github.com/graphql-go/graphql"
+
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestCombineObjectConfig(t *testing.T) {
 	Convey("Given multiple graphql.ObjectConfig's", t, func() {
+		config1 := graphql.ObjectConfig{
+			Name: "Name1",
+			Fields: graphql.Fields{
+				"Test1": &graphql.Field{
+					Name: "Test1",
+				},
+				"Test2": &graphql.Field{
+					Name: "Test2",
+				},
+			},
+		}
+		config2 := graphql.ObjectConfig{
+			Name: "Name1",
+			Fields: graphql.Fields{
+				"Test3": &graphql.Field{
+					Name: "Test3",
+				},
+				"Test4": &graphql.Field{
+					Name: "Test4",
+				},
+				"Test5": &graphql.Field{
+					Name: "Test5",
+				},
+			},
+		}
+		combinedConfig := combineObjectConfig(config1, config2)
+		Convey("The graphql.ObjectConfig returned should contain all the fields of the passed in configs", func() {
+			fields := combinedConfig.Fields.(graphql.Fields)
+			_, firstIsOk := fields["Test1"]
+			_, secondIsOk := fields["Test2"]
+			_, thirdIsOk := fields["Test3"]
+			_, fourthIsOk := fields["Test4"]
+			_, fifthIsOk := fields["Test5"]
 
+			So(firstIsOk, ShouldBeTrue)
+			So(secondIsOk, ShouldBeTrue)
+			So(thirdIsOk, ShouldBeTrue)
+			So(fourthIsOk, ShouldBeTrue)
+			So(fifthIsOk, ShouldBeTrue)
+		})
 	})
 }
