b9c19b54499e8098d747b2525136d6e5b3289951
Felix Wieland <felix.wieland@mail.de>
2020-01-26 22:10:40 +0100

    added util tests

--- util_test.go
+++ util_test.go
@@ -3,12 +3,30 @@ package kosmo
 import (
 	"testing"
 
+	"github.com/graphql-go/graphql"
+
 	. "github.com/smartystreets/goconvey/convey"
 )
 
+type TStructForObjectConfigTest struct {
+	Name string
+}
+
 func TestReplaceResolverPrefixes(t *testing.T) {
 	Convey("Given prefixes and graphql.Fields", t, func() {
+		fields := graphql.Fields{
+			"Pre1Name1": &graphql.Field{},
+			"Pre2Name2": &graphql.Field{},
+		}
+		prefixes := []string{"Pre1", "Pre2"}
+		newFields := replaceResolverPrefixes(prefixes, fields)
+		Convey("The matching prefixes in the given fields should have been replaced with an empty string", func() {
+			_, ok1 := newFields["Name1"]
+			_, ok2 := newFields["Name2"]
 
+			So(ok1, ShouldBeTrue)
+			So(ok2, ShouldBeTrue)
+		})
 	})
 }
 
@@ -34,12 +52,37 @@ func TestValidateResolverArgument(t *testing.T) {
 
 func TestGqlObjFallbackFactory(t *testing.T) {
 	Convey("Given a graphql.ObjectConfig", t, func() {
-
+		conf := structToGraphConfig(TStructForObjectConfigTest{})
+		fn := gqlObjFallbackFactory(conf)
+		Convey("It should return a cache setter function", func() {
+			Convey("And the setter should receive a new graphql.Object", func() {
+				obj := graphql.NewObject(conf)
+				setter := func(value interface{}) {
+					So(value, ShouldHaveSameTypeAs, obj)
+					So(value, ShouldNotBeNil)
+				}
+				fn(setter)
+			})
+		})
 	})
 }
 
 func TestMakeGraphQLObject(t *testing.T) {
 	Convey("Given a graphql.ObjectConfig", t, func() {
-
+		conf := structToGraphConfig(TStructForObjectConfigTest{})
+		Convey("In case the config name is empty", func() {
+			confCopy := conf
+			confCopy.Name = ""
+			obj := makeGraphQLObject(confCopy)
+			Convey("It should return nil", func() {
+				So(obj, ShouldBeNil)
+			})
+		})
+		Convey("In case the config name is set", func() {
+			obj := makeGraphQLObject(conf)
+			Convey("It should return a valid graphql.Object", func() {
+				So(obj, ShouldNotBeNil)
+			})
+		})
 	})
 }
