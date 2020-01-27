b383a18a6cd799b1490cdf82435f241ebca91363
Felix Wieland <felix.wieland@mail.de>
2020-01-27 23:49:22 +0100

    added server tests

--- server_test.go
+++ server_test.go
@@ -6,8 +6,27 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestMuxServer(t *testing.T) {
-	Convey("Given a HTTPConfig and a graphql.Schema", t, func() {
+type TMuxServerSchemaStruct struct {
+	Name string
+}
 
+func TResolveMuxServerSchema() (TMuxServerSchemaStruct, error) {
+	return TMuxServerSchemaStruct{
+		Name: "Test",
+	}, nil
+}
+
+func TestMuxServer(t *testing.T) {
+	svc := Service{
+		HTTPConfig: HTTPConfig{
+			Port: ":8080",
+		},
+	}
+	tMux := Type(TMuxServerSchemaStruct{}).Query(TResolveMuxServerSchema)
+	Convey("Given a HTTPConfig and a graphql.Schema", t, func() {
+		server := svc.Schemas(tMux).Server()
+		Convey("The returned server should have the port 8080", func() {
+			So(server.Addr, ShouldEqual, ":8080")
+		})
 	})
 }
